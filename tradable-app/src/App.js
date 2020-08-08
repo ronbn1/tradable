66e7165fb9cabe9138e8041c013449549dfbed89
Ron Benaish <r.benaish@gmail.com>
2020-08-08 15:59:59 +0300

    test

---
+++ tradable-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import { Title } from "rb-ui";
+
+function App() {
+   return (
+      <>
+         <Divv>hello</Divv>
+         <Title text="Hello World" />;
+      </>
+   );
+}
+
+const Divv = styled.div``;
+
+export default App;
